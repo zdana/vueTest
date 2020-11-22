3b9f379d783d70a7f8690b7d36403821484dd5c2
zdana <dana_zhao@foxmail.com>
2020-11-22 22:37:08 +0800

    修改为html的history模式

--- vueTest/src/router/index.js
+++ vueTest/src/router/index.js
@@ -6,7 +6,12 @@ import About from '@/components/About'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',//将hash路径改为history，即取代哦浏览器url中的#变成html模式
   routes: [
+    {
+      path: '',
+      redirect:'/home'//redirect,重定向，即当访问‘’时定向到home中
+    },
     {
       path: '/home',
       name: 'Home',
