9bce5542e32a0e3085c80f9af3ebfa2807db0e72
zdana <dana_zhao@foxmail.com>
2020-11-23 22:01:11 +0800

    this.$router

--- vueTest/src/App.vue
+++ vueTest/src/App.vue
@@ -8,10 +8,13 @@
       属性4. active-class:点击时调用actice这个class，该方法的缺陷是每当需要逐个给标签添加 active-class="active"，
             替代方法: 在router中加入：linkActiveClass:'active'
     -->
+    <!-- 路由跳转方法一 -->
     <!-- <router-link to="/home" tag="button" replace="" active-class="active">首页</router-link>
     <router-link to="/about" active-class="active">关于</router-link> -->
-    <router-link to="/home" tag="button" replace="">首页</router-link>
-    <router-link to="/about">关于</router-link>
+   
+    <!-- 路由跳转方法二 -->
+    <button @click="homeClick">首页</button>
+    <button @click="aboutClick">关于</button>
     <!--  <router-view/>表示占位，根据当前路径动态渲染不同组件-->
      <router-view/>
   </div>
@@ -19,7 +22,19 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods:{
+    // 通过代码的方式修改路由
+    homeClick(){
+      // push相当于pushstate
+      // this.$router.push('/home')
+      this.$router.replace('/home')
+    },
+    aboutClick(){
+      // this.$router.push('/about')
+      this.$router.replace('/about')
+    }
+  }
 }
 </script>
 
