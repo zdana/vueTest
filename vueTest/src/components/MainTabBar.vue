a94e178d6c40c3225ef5e0327a61aab26b0157a1
zdana <dana_zhao@foxmail.com>
2020-12-07 21:50:38 +0800

    tabbar封装

---
+++ vueTest/src/components/MainTabBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item link="/home" activeColor="pink">
+        <img slot="item-icon" src="../../static/img/TabBar/zhuye 2.svg" alt />
+        <img slot="item-icon-active" src="../../static/img/TabBar/zhuye.svg" alt />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item link="/category" activeColor="red">
+        <img slot="item-icon" src="../../static/img/TabBar/fenlei 2.svg" alt />
+        <img slot="item-icon-active" src="../../static/img/TabBar/fenlei.svg" alt />
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item link="/cart" activeColor="yellow">
+        <img slot="item-icon" src="../../static/img/TabBar/gouwuche 2.svg" alt />
+        <img slot="item-icon-active" src="../../static/img/TabBar/gouwuche.svg" alt />
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item link="/profile" activeColor="yellowGreen">
+        <img slot="item-icon" src="../../static/img/TabBar/wode 2.svg" alt />
+        <img slot="item-icon-active" src="../../static/img/TabBar/wode.svg" alt />
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+<script>
+import TabBar from '../components/tabbar/TabBar'
+import TabBarItem from '../components/tabbar/TabBarItem'
+
+export default{
+  name:'MainTabBar',
+    components:{
+    TabBar,
+    TabBarItem
+  },
+}
+</script>
